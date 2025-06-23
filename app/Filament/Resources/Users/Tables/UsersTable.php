237fcbb88f94c5a4a30c85dfc0574a0a3f9522c3
Ivan Boldyrev <iboldurev@gmail.com>
2025-06-23 13:08:49 +0300

    added filters

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -9,11 +9,15 @@ use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Forms\Components\DatePicker;
+use Filament\Schemas;
 use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\Width;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class UsersTable
 {
@@ -56,6 +60,34 @@ class UsersTable
                     ->label('Status')
                     ->options(Enums\UserStatus::class)
                     ->native(false),
+                Filter::make('created_at')
+                    ->columnSpanFull()
+                    ->schema([
+                        Schemas\Components\FusedGroup::make([
+                            DatePicker::make('from')
+                                ->native(false)
+                                ->label('Created From')
+                                ->placeholder('From'),
+
+                            DatePicker::make('until')
+                                ->native(false)
+                                ->label('Created Until')
+                                ->placeholder('Until'),
+                        ])
+                            ->label('Created At')
+                            ->columns(columns: 2),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['from'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['until'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    }),
             ])
             ->recordActions([
                 ViewAction::make()
